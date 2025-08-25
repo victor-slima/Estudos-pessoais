5e81e060d6f0c34bf13b19bb578c9772d9a93670
victor.slima <victorslima.dev@gmail.com>
2025-08-25 13:29:17 -0300

    inserindo os atributos prox_pos_vazia, prim e ult.

--- Lista estatica encadeada/Lee.py
+++ Lista estatica encadeada/Lee.py
@@ -29,3 +29,36 @@ class No:
         # segundo atributo, que será o ponteiro:
         self.prox = proximo
 
+# Agora precisaremos criar uma nova classe para a lista, que chamaremos de Lee:
+# CRIANDOA ESTRUTURA DA CLASSE Lee:
+
+class Lee:
+    
+    # Iniciando o método construtor, e inserindo seus parâmetros e atributos.
+    def __init__(self, tamanho): # tamanho = tamanho da lista
+        
+        # atributo que será o tamanho máximo da lista:
+        self.tam_maximo = tamanho
+
+        # atributo que receberá uma lista com o valor None vezes o tamanho:
+        self.vetor = [None] * self.tam_maximo
+
+        # atributo que receberá a quantidade de elementos dentro da lista:
+        self.quant = 0  # iniciando com zero elementos, lista vazia.
+
+        # Agora temos mais atributos:
+        
+        # atributo que guarda o índice da próxima posição livre no vetor,
+        # inicialmente, aponta para o primeiro elemento livre, (0),
+        # e serve para você saber onde inserir o próximo nó sem precisar procurar pelo vetor inteiro:
+        self.prox_pos_vazia = self.inicializar_estrutura()  # "onde posso colocar o proximo elemento"
+
+        # atributo que guarda o índice do primeiro elemento válido da lista:
+        # incialmente, a lista está vazia, então prim inicia valendo -1
+        # quando voce insere o primeiro elemento, prim passa apontar para o índice desse nó:
+        self.prim = -1
+
+        # atributo que guarda o último elemento válido da lista.
+        # Inicialmente também é -1 (lista vazia).
+        # Quando inserimos, ult ajuda a manter referencia ao final da lista.
+        self.ult = -1   
