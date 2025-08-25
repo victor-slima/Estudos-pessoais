57c7ec0d24c8ea1485e3b6aa4d5350c4509936eb
victor.slima <victorslima.dev@gmail.com>
2025-08-25 12:47:32 -0300

    inserindo a classe No, e seus atributos.

--- Lista estatica encadeada/Lee.py
+++ Lista estatica encadeada/Lee.py
@@ -15,3 +15,17 @@
     # Quando se precisa de flexibilidade na inserção/remoção;
     # Quando a realocação de elementos de uma lista sequencial, seria custosa.
 
+# CRIANDO A ESTRUTURA DO NÓ:
+    # Primeiro, criamos uma classe simples, que chamaremos de "No", que contém 2 atributos:
+    # info => armazenará o valor;
+    # prox => cuidará do endereço do proximo elemento na lista.
+
+class No:
+
+    # Inciando o método construtor, e inserindo os parametros que os atributos irão receber:
+    def __init__(self, valor, proximo):
+        # primeiro atributo, que será o valor do elemento:
+        self.info = valor
+        # segundo atributo, que será o ponteiro:
+        self.prox = proximo
+
